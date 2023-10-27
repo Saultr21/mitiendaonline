7b85cd1cebe667639b54c2bde0e317014173db30
Saultr21 <saultomastrujillorodriguez@alumno.ieselrincon.es>
2023-10-27 10:29:55 +0100

    Config y menu principal

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menú Principal</title>
+</head>
+<body>
+    <h1>Menú Principal</h1>
+    <ul>
+        <li><a href="crear_producto.php">Crear Producto</a></li>
+        <li><a href="listado_productos.php">Consultar el Listado de Productos</a></li>
+        <li><a href="modifica_producto.php">Modificar Producto</a></li>
+        <li><a href="elimina_producto.php">Eliminar Producto</a></li>
+    </ul>
+</body>
+</html>
+
+
