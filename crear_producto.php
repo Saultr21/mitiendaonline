fb926c8412580a1c62b78a2ece4bfbaa2cbd74cc
Saultr21 <saultomastrujillorodriguez@alumno.ieselrincon.es>
2023-10-30 12:19:13 +0000

    Okey

--- crear_producto.php
+++ crear_producto.php
@@ -57,12 +57,23 @@ try {
     <title>Listado de Productos</title>
 </head>
 <body>
+<div class="container mt-5">
     <form action="crear_producto.php" method="post">
-        Nombre: <input type="text" name="nombre" value="<?php echo htmlspecialchars($nombre); ?>"><br>
-        Precio: <input type="text" name="precio" value="<?php echo htmlspecialchars($precio); ?>"><br>
-        Imagen: <input type="text" name="imagen" value="<?php echo htmlspecialchars($imagen); ?>"><br>
-        Categoría:
-        <select name="categoria">
+    <div class="form-group">
+        <label for="Nombre">Nombre: </label>
+        <input class="form-control" type="text" name="nombre" value="<?php echo htmlspecialchars($nombre); ?>">
+    </div>
+    <div class="form-group">
+        <label for="Nombre">Precio: </label>
+        <input class="form-control" type="text" name="precio" value="<?php echo htmlspecialchars($precio); ?>">
+    </div>
+    <div class="form-group">
+        <label for="Nombre">Imagen: </label>
+        <input class="form-control" type="text" name="imagen" value="<?php echo htmlspecialchars($imagen); ?>">
+    </div>
+    <div class="form-group">
+        <label for="Nombre">Categoria: </label>
+        <select class="form-control" id="Categoria" name="Categoria" required>
             <option value="">Selecciona una categoría</option>
             <?php foreach ($categorias as $cat): ?>
                 <option value="<?php echo htmlspecialchars($cat['id']); ?>" <?php echo $cat['id'] === $categoria ? "selected" : ""; ?>>
@@ -70,9 +81,13 @@ try {
                 </option>
             <?php endforeach; ?>
         </select><br>
-        <input type="submit" value="Crear Producto">
-    </form>
+    </div>
+        <input class="btn btn-primary" type="submit" value="Crear Producto">
+        
+
     <a class="btn btn-primary" href="index.php">Volver al menú</a>
+    </form>
+</div>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
--- modifica_producto.php
+++ modifica_producto.php
@@ -47,10 +47,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     echo "<script>alert('Producto actualizado correctamente');</script>";
 }
+
+$categorias = [];
+try {
+    $stmt = $conn->query("SELECT id, nombre FROM categorias");
+    $categorias = $stmt->fetchAll(PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    echo "<p>Error: " . $e->getMessage() . "</p>";
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -58,6 +67,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
     <title>Editar Producto</title>
 </head>
+
 <body>
     <div class="container mt-5">
         <h1>Editar Producto</h1>
@@ -68,7 +78,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 <label for="producto">Seleccionar producto:</label>
                 <select class="form-control" id="producto" name="id" onchange="this.form.submit()">
                     <option value="">-- Seleccionar --</option>
-                    <?php foreach ($listaProductos as $p): ?>
+                    <?php foreach ($listaProductos as $p) : ?>
                         <option value="<?= $p['id'] ?>" <?= $producto && $producto['id'] == $p['id'] ? 'selected' : '' ?>>
                             <?= htmlspecialchars($p['Nombre']) ?>
                         </option>
@@ -78,33 +88,42 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         </form>
 
         <!-- Formulario para editar el producto -->
-        <?php if ($producto): ?>
-        <form method="post">
-            <input type="hidden" name="id" value="<?= $producto['id'] ?>">
-            <div class="form-group">
-                <label for="Nombre">Nombre:</label>
-                <input type="text" class="form-control" id="Nombre" name="Nombre" value="<?= htmlspecialchars($producto['Nombre']) ?>" required>
-            </div>
-            <div class="form-group">
-                <label for="Precio">Precio:</label>
-                <input type="number" class="form-control" id="Precio" name="Precio" value="<?= htmlspecialchars($producto['Precio']) ?>" required>
-            </div>
-            <div class="form-group">
-                <label for="Imagen">Imagen:</label>
-                <input type="text" class="form-control" id="Imagen" name="Imagen" value="<?= htmlspecialchars($producto['Imagen']) ?>">
-            </div>
-            <div class="form-group">
-                <label for="Categoria">Categoría:</label>
-                <input type="text" class="form-control" id="Categoria" name="Categoria" value="<?= htmlspecialchars($producto['Categoria']) ?>" required>
-            </div>
-            <button type="submit" class="btn btn-primary">Guardar cambios</button>
-        </form>
+        <?php if ($producto) : ?>
+            <form method="post">
+                <input type="hidden" name="id" value="<?= $producto['id'] ?>">
+                <div class="form-group">
+                    <label for="Nombre">Nombre:</label>
+                    <input type="text" class="form-control" id="Nombre" name="Nombre" value="<?= htmlspecialchars($producto['Nombre']) ?>" required>
+                </div>
+                <div class="form-group">
+                    <label for="Precio">Precio:</label>
+                    <input type="number" class="form-control" id="Precio" name="Precio" value="<?= htmlspecialchars($producto['Precio']) ?>" required>
+                </div>
+                <div class="form-group">
+                    <label for="Imagen">Imagen:</label>
+                    <input type="text" class="form-control" id="Imagen" name="Imagen" value="<?= htmlspecialchars($producto['Imagen']) ?>">
+                </div>
+                <div class="form-group">
+                    <label for="Categoria">Categoría:</label>
+                    <select class="form-control" id="Categoria" name="Categoria" required>
+                        <option value="">Selecciona una categoría</option>
+                        <?php foreach ($categorias as $cat) : ?>
+                            <option value="<?= htmlspecialchars($cat['id']); ?>" <?= $cat['id'] == $producto['Categoria'] ? 'selected' : '' ?>>
+                                <?= htmlspecialchars($cat['nombre']); ?>
+                            </option>
+                        <?php endforeach; ?>
+                    </select>
+                </div>
+
+                <button type="submit" class="btn btn-primary">Guardar cambios</button>
+            </form>
         <?php endif; ?>
-        
+
         <a class="btn btn-secondary mt-3" href="listado_productos.php">Volver al listado</a>
     </div>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </body>
+
 </html>
